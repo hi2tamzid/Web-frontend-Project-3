2de449d82f0af6a2a8407bc3b9017c91d5c48d27
Tamzid <tamzidmahmud2@gmail.com>
2021-01-26 20:16:50 +0600

    - All designed completed * Final Check Up needed

--- js/code.js
+++ js/code.js
@@ -1,11 +1,26 @@
 function changeactive(e){
   if(e == 1){
     document.getElementById("aboutus").classList.remove("text-info");
+    document.getElementById("servicesid").classList.remove("text-info");
+    document.getElementById("contactid").classList.remove("text-info");
     document.getElementById("homeid").classList.add("text-info");
   }
-  else{
+  else if(e == 2){
     document.getElementById("homeid").classList.remove("text-info");
     document.getElementById("aboutus").classList.add("text-info");
+    document.getElementById("servicesid").classList.remove("text-info");
+    document.getElementById("contactid").classList.remove("text-info");
   }
-  
+  else if(e == 3){
+    document.getElementById("homeid").classList.remove("text-info");
+    document.getElementById("aboutus").classList.remove("text-info");
+    document.getElementById("servicesid").classList.add("text-info");
+    document.getElementById("contactid").classList.remove("text-info");
+  }
+  else if(e == 4){
+    document.getElementById("homeid").classList.remove("text-info");
+    document.getElementById("aboutus").classList.remove("text-info");
+    document.getElementById("servicesid").classList.remove("text-info");
+    document.getElementById("contactid").classList.add("text-info");
+  }
 }
